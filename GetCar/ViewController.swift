74e48857c1241d99a82dd92dbc46add7e9769851
Vinay <Vinayshankar07@gmail.com>
2020-05-23 16:46:05 +0530

    add ViewwillAppear method

--- GetCar/ViewController.swift
+++ GetCar/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+    }
 
 }
 
